b015fd1209a7091659c83edc1536418960c2e857
Nathan Smith <nathanielvs@gmail.com>
2020-03-25 15:53:38 -0600

    Add image credits.

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ var time = new Date().getHours();
 var wakeUpTime = 6; // 6am
 var meal1 = 7; // 7am
 var yogaTime = 8; // 8am
-var meal2 = 12; // 12pm
+var meal2 = 1; // 12pm
 var gymTime = 13; // 1pm
-var meal3 = 15; // 3pm
+var meal3 = 12; // 3pm
 var meal4 = 18; // 6pm
 var meal5 = 20; // 8pm
 var bedTime = 22; // 10pm
@@ -16,11 +16,13 @@ var beachTime;
 var updateClock = function() {
   // Assign JS variables to HMTL elements
   var fitnessImageJS = document.getElementById("fitnessImage");
+  var creditJS = document.getElementById("credit");
   var eventTitleJS = document.getElementById("eventTitle");
   var timeEventJS = document.getElementById("timeEvent");
 
   // Placeholder variables for dynamic content
   var messageImage;
+  var imageCredit;
   var messageTitle;
   var messageText;
 
@@ -29,65 +31,78 @@ var updateClock = function() {
     messageTitle = "Shut it down";
     messageText = "Stop looking at the clock & go to sleep.";
     messageImage = "img/sleepTime.png";
+    imageCredit = "Calum von Moger";
 
   } else if (time == wakeUpTime) {
     messageTitle = "Wake Up";
     messageText = "Good morning, Starshine. The earth says hello.";
     messageImage = "img/wakeUpTime.jpg";
+    imageCredit = "Simeon Panda";
 
   } else if (time == meal1) {
     messageTitle = "Meal #1";
     messageText = "Brekkie.";
     messageImage = "img/meal1.JPG";
+    imageCredit = "Pexels - Lum3n.com";
 
   } else if (time == yogaTime) {
     messageTitle = "Yoga Time";
     messageText = "Get down with the dog.";
     messageImage = "img/yogaTime.jpg";
+    imageCredit = "Pixabay - Peggy Marco";
 
   } else if (time == meal2) {
     messageTitle = "Meal #2";
     messageText = "Fuel up for the gym!";
     messageImage = "img/meal2.jpg";
+    imageCredit = "Pexels - Ella Olsson";
 
   } else if (time == gymTime) {
     messageTitle = "Gym Time";
     messageText = "Date with the weights. Get to it!";
     messageImage = "img/gymTime.jpg";
+    imageCredit = "Jonathan Delarosa & Flex Lewis";
 
   } else if (time == meal3) {
     messageTitle = "Meal #3";
     messageText = "Refuel. You've earned it, bud.";
     messageImage = "img/meal3.jpg";
+    imageCredit = "Pexels - Ella Olsson";
 
   } else if (time == meal4) {
     messageTitle = "Meal #4";
     messageText = "Time to eat again.";
     messageImage = "img/meal4.jpg";
+    imageCredit = "Pexels - Kaboompics.com";
 
   } else if (time == meal5) {
     messageTitle = "Meal #5";
     messageText = "Chow down one last time.";
     messageImage = "img/meal5.jpg";
+    imageCredit = "Pexels - Valeria Boltneva";
 
   } else if (time == bedTime) {
     messageTitle = "Bed Time";
     messageText = "Early to bed & early to rise, builds up your biceps, triceps, and thighs.";
     messageImage = "img/bedTime.jpg";
+    imageCredit = "Matt Evans";
 
   } else if (time == beachTime) {
     messageTitle = "Surfs Up";
     messageText = "Have some fun in the sun. Because beaches, that's why.";
     messageImage = "img/beachTime.gif";
+    imageCredit = "MJ Studios - Diego Sechi";
 
   } else {
     messageTitle = "Free Time";
     messageText = "Nothing scheduled right now. You're free as a bird.";
     messageImage = "img/fitness.jpg";
+    imageCredit = "Pexels - theformfitness";
   }
 
   // Link timeEvent dynamic content to JS variable properties
   fitnessImageJS.src = messageImage;
+  creditJS.innerText = imageCredit;
   eventTitleJS.innerText = messageTitle;
   timeEventJS.innerText = messageText;
 
