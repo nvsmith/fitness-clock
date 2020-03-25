7bf89018251eb3e2fc87a5c45a2bf315660dde6c
Nathan Smith <nathanielvs@gmail.com>
2020-03-25 12:00:02 -0600

    Final code cleanups, CSS sort. Backup file updates.

--- js/main-backup.js
+++ js/main-backup.js
@@ -14,66 +14,81 @@ var bedTime = 22; // 10pm
 var beachTime;
 
 var updateClock = function() {
-  // Link JS variables to HMTL elements
-  var fitnessImageJS = document.getElementById('fitnessImage');
-  var timeEventJS = document.getElementById('timeEvent');
+  // Assign JS variables to HMTL elements
+  var fitnessImageJS = document.getElementById("fitnessImage");
+  var eventTitleJS = document.getElementById("eventTitle");
+  var timeEventJS = document.getElementById("timeEvent");
 
   // Placeholder variables for dynamic content
   var messageImage;
+  var messageTitle;
   var messageText;
 
-  // Assign dynamic text/image variables to time events
+  // Assign dynamic variables to time events
   if (time < wakeUpTime || time > bedTime) {
-    messageText = "BEDTIME \n\n If you're reading this, stop looking at the clock & go to sleep.";
-    messageImage = "img/sleepTime.png"
+    messageTitle = "Shut it down";
+    messageText = "Stop looking at the clock & go to sleep.";
+    messageImage = "img/sleepTime.png";
 
   } else if (time == wakeUpTime) {
-    messageText = "WAKE UP \n\n Good morning, Starshine. The earth says hello.";
-    messageImage = "img/wakeUpTime.jpg"
+    messageTitle = "Wake Up";
+    messageText = "Good morning, Starshine. The earth says hello.";
+    messageImage = "img/wakeUpTime.jpg";
 
   } else if (time == meal1) {
-    messageText = "MEAL #1 \n\n Brekkie.";
-    messageImage = "img/meal1.JPG"
+    messageTitle = "Meal #1";
+    messageText = "Brekkie.";
+    messageImage = "img/meal1.JPG";
 
   } else if (time == yogaTime) {
-    messageText = "YOGA TIME \n\n Get down with the dog.";
-    messageImage = "img/yogaTime.jpg"
+    messageTitle = "Yoga Time";
+    messageText = "Get down with the dog.";
+    messageImage = "img/yogaTime.jpg";
 
   } else if (time == meal2) {
-    messageText = "MEAL #2 \n\n Fuel up for the gym!";
-    messageImage = "img/meal2.jpg"
+    messageTitle = "Meal #2";
+    messageText = "Fuel up for the gym!";
+    messageImage = "img/meal2.jpg";
 
   } else if (time == gymTime) {
-    messageText = "GYM TIME \n\n Date with the weights. Get to it!";
-    messageImage = "img/gymTime.jpg"
+    messageTitle = "Gym Time";
+    messageText = "Date with the weights. Get to it!";
+    messageImage = "img/gymTime.jpg";
 
   } else if (time == meal3) {
-    messageText = "MEAL #3 \n\n Refuel. You've earned it, bud.";
-    messageImage = "img/meal3.jpg"
+    messageTitle = "Meal #3";
+    messageText = "Refuel. You've earned it, bud.";
+    messageImage = "img/meal3.jpg";
 
   } else if (time == meal4) {
-    messageText = "MEAL #4 \n\n Time to eat again.";
-    messageImage = "img/meal4.jpg"
+    messageTitle = "Meal #4";
+    messageText = "Time to eat again.";
+    messageImage = "img/meal4.jpg";
 
   } else if (time == meal5) {
-    messageText = "MEAL #5 \n\n Chow down one last time.";
-    messageImage = "img/meal5.jpg"
+    messageTitle = "Meal #5";
+    messageText = "Chow down one last time.";
+    messageImage = "img/meal5.jpg";
 
   } else if (time == bedTime) {
-    messageText = "BEDTIME \n\n Early to bed, early to rise, builds up your biceps, triceps, and thighs.";
-    messageImage = "img/bedTime.jpg"
+    messageTitle = "Bed Time";
+    messageText = "Early to bed & early to rise, builds up your biceps, triceps, and thighs.";
+    messageImage = "img/bedTime.jpg";
 
   } else if (time == beachTime) {
-    messageText = "Fun in the sun.\n\n Because beaches, that's why.";
+    messageTitle = "Surfs Up";
+    messageText = "Have some fun in the sun. Because beaches, that's why.";
     messageImage = "img/beachTime.gif";
 
   } else {
-    messageText = "Nothing scheduled right now.\n\n You're free as a bird.";
-    messageImage = "img/fitness.jpg"
+    messageTitle = "Free Time";
+    messageText = "Nothing scheduled right now. You're free as a bird.";
+    messageImage = "img/fitness.jpg";
   }
 
   // Link timeEvent dynamic content to JS variable properties
   fitnessImageJS.src = messageImage;
+  eventTitleJS.innerText = messageTitle;
   timeEventJS.innerText = messageText;
 
   showCurrentTime();
@@ -82,7 +97,7 @@ var updateClock = function() {
 // Show the current time
 var showCurrentTime = function() {
   // Link JS variables to HMTL elements
-  var clockJS = document.getElementById('clock');
+  var clockJS = document.getElementById("clock");
 
   // Fetch the time
   var currentTime = new Date();
@@ -124,7 +139,7 @@ var oneSecond = 1000; // 1000 JS miliseconds per 1 second
 var myInterval = setInterval(updateClock, oneSecond);
 
 // Buttons & selectors to change dynamic message text/images
-var beachTimeButtonJS = document.getElementById('beachTimeButton');
+var beachTimeButtonJS = document.getElementById("beachTimeButton");
 var wakeUpTimeSelectorJS = document.getElementById("wakeUpTimeSelector");
 var lunchTimeSelectorJS = document.getElementById("lunchTimeSelector");
 var bedTimeSelectorJS = document.getElementById("bedTimeSelector");
@@ -158,7 +173,7 @@ var bedTimeEvent = function() {
 };
 
 // Event listeners for all buttons
-beachTimeButtonJS.addEventListener("click", beachEvent)
-wakeUpTimeSelectorJS.addEventListener('change', wakeUpEvent);
-lunchTimeSelectorJS.addEventListener('change', lunchEvent);
-bedTimeSelectorJS.addEventListener('change', bedTimeEvent);
+beachTimeButtonJS.addEventListener("click", beachEvent);
+wakeUpTimeSelectorJS.addEventListener("change", wakeUpEvent);
+lunchTimeSelectorJS.addEventListener("change", lunchEvent);
+bedTimeSelectorJS.addEventListener("change", bedTimeEvent);
